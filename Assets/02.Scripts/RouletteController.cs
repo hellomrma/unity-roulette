da964bf84d50b61e9ae84111ef33faad0144f612
Noah Ma <hellomrma@hivelab.co.kr>
2025-12-14 12:46:54 +0900

    Update RouletteController.cs

--- Assets/02.Scripts/RouletteController.cs
+++ Assets/02.Scripts/RouletteController.cs
@@ -6,6 +6,9 @@ public class RouletteController : MonoBehaviour
 {
     public float rotateSpeed = 0f;
 
+    public bool isRotating = false;
+    public bool isRotateEnded = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,22 +18,33 @@ public class RouletteController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && isRotating == false && isRotateEnded == true)
         {
-            rotateSpeed = 10f;
+            // rotateSpeed = 10f;
+
+            isRotating = true;
+            isRotateEnded = false;
+
+            rotateSpeed = Random.Range(7f, 15f);
+
+            // Unity Random 함수
+            // float Random.Range(float minInclusive, float maxInclusive);
+            // int Random.Range(int minInclusive, int maxExclusive);
         }
         transform.Rotate(0,0, rotateSpeed);
 
-        rotateSpeed *= 0.996f;
-        Debug.Log(rotateSpeed);
+        rotateSpeed *= 0.98f;
+        //Debug.Log(rotateSpeed);
 
-        if (rotateSpeed <= 0.1f)
+        if (rotateSpeed <= 0.05f)
         {
             rotateSpeed = 0f;
-            Debug.Log("정지됨");
+
+            isRotateEnded = true;
+            isRotating = false;
         }
         else { 
-            Debug.Log("회전중");
+
         }
     }
 }
