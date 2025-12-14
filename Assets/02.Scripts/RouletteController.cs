eae7278ead8e74a0a9d9b9c3ed23603bdfc9a1ef
Noah Ma <hellomrma@hivelab.co.kr>
2025-12-14 10:59:01 +0900

    Init

---
+++ Assets/02.Scripts/RouletteController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotateSpeed = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rotateSpeed = 10f;
+        }
+        transform.Rotate(0,0, rotateSpeed);
+    }
+}
